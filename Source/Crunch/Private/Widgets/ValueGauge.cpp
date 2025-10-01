b36d10ea31ea7396c4a745cfcfc5073d3069faaa
Yasusu <138525886+YasusuDev@users.noreply.github.com>
2025-10-01 14:42:24 -0300

    Update ValueGauge.cpp

--- Source/Crunch/Private/Widgets/ValueGauge.cpp
+++ Source/Crunch/Private/Widgets/ValueGauge.cpp
@@ -1,10 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ValueGauge.h"
-
 #include "Components/ProgressBar.h"
 #include "Components/TextBlock.h"
-#include "ProfilingDebugging/CookStats.h"
 
 void UValueGauge::NativePreConstruct()
 {
