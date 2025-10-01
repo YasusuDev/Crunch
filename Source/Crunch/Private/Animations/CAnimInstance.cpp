0f6e7017180bc754e56a4b515a900f3b4dca9e32
Yasusu <138525886+YasusuDev@users.noreply.github.com>
2025-10-01 00:02:25 -0300

    feat: added move, jump and lean animations.

---
+++ Source/Crunch/Private/Animations/CAnimInstance.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+
+	if (IsValid(OwnerCharacter))
+	{
+		OwnerMovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(OwnerCharacter))
+	{
+		Speed = OwnerCharacter->GetVelocity().Length();
+
+		FRotator BodyRotation = OwnerCharacter->GetActorRotation();
+		FRotator BodyRotationDelta = UKismetMathLibrary::NormalizedDeltaRotator(BodyRotation, BodyPreviousRotation);
+		BodyPreviousRotation = BodyRotation;
+
+		YawSpeed = BodyRotationDelta.Yaw / DeltaSeconds;
+		SmoothedYawSpeed = UKismetMathLibrary::FInterpTo(SmoothedYawSpeed, YawSpeed, DeltaSeconds, YawSpeedSmoothLerpSpeed);
+	}
+
+	if (IsValid(OwnerMovementComponent))
+	{
+		bIsJumping = OwnerMovementComponent->IsFalling();
+	}
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+}
